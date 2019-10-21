e7ab5650fed6f85b1c730567b591c880471ca1fd
fengshuan <1263215592@qq.com>
2019-10-21 22:34:40 +0800

    :sparkles:使用koa-json-error 进行错误处理

--- app/index.js
+++ app/index.js
@@ -1,18 +1,27 @@
 const Koa = require("koa");
 const app = new Koa();
 const bodyParser = require("koa-bodyparser");
+const error = require("koa-json-error");
 const routing = require("./routes");
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.status = err.status || err.statusCode || 500;
-    ctx.body = {
-      message: err.message
-    };
-  }
-});
+// 自定义简易版错误处理
+// app.use(async (ctx, next) => {
+//   try {
+//     await next();
+//   } catch (err) {
+//     ctx.status = err.status || err.statusCode || 500;
+//     ctx.body = {
+//       message: err.message
+//     };
+//   }
+// });
+
+app.use(
+  error({
+    postFormat: (e, { stack, ...rest }) =>
+      process.env.NODE_ENV === "production" ? rest : { stack, ...rest }
+  })
+);
 app.use(bodyParser());
 
 routing(app);
