eed9cfdd7af22ed205ee8be29394ac2d59883ff3
fengshuan <1263215592@qq.com>
2019-10-25 22:20:03 +0800

    :sparkles: mongoose实现用户模块crud

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,37 +1,37 @@
-const db = [{ name: "杜仲" }];
-
+const User = require("../models/users");
 class UserController {
-  find(ctx) {
-    ctx.body = db;
+  async find(ctx) {
+    ctx.body = await User.find();
   }
-  findById(ctx) {
-    if (ctx.params.id * 1 >= db.length) {
-      ctx.throw(412, "先决条件失败：id大于等于数组长度");
+  async findById(ctx) {
+    const user = await User.findById(ctx.params.id);
+    if (!user) {
+      ctx.throw(404, "用户不存在");
     }
-    ctx.body = db[ctx.params.id * 1];
+    ctx.body = user;
   }
-  create(ctx) {
+  async create(ctx) {
     ctx.verifyParams({
       name: { type: "string", required: true }
     });
-    db.push(ctx.request.body);
-    ctx.body = ctx.request.body;
+    const user = await new User(ctx.request.body).save();
+    ctx.body = user;
   }
-  update(ctx) {
-    if (ctx.params.id * 1 >= db.length) {
-      ctx.throw(412, "先决条件失败：id大于等于数组长度");
-    }
+  async update(ctx) {
     ctx.verifyParams({
       name: { type: "string", required: true }
     });
-    db[ctx.params.id * 1] = ctx.request.body;
-    ctx.body = ctx.request.body;
-  }
-  delete(ctx) {
-    if (ctx.params.id * 1 >= db.length) {
-      ctx.throw(412, "先决条件失败：id大于等于数组长度");
+    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+    if (!user) {
+      ctx.throw(404, "用户不存在");
     }
-    db.splice(ctx.params.id * 1, 1);
+    ctx.body = user;
+  }
+  async delete(ctx) {
+    const user = await User.findByIdAndRemove(ctx.params.id);
+    if (!user) {
+      ctx.throw(404, "用户不存在");
+    }
     ctx.status = 204;
   }
 }
