9d5b9d8fdb52c925434e7b64881f44c4fdfe7dc0
fengshuan <1263215592@qq.com>
2019-11-02 22:15:42 +0800

    :sparkles: 个人资料参数校验

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -34,7 +34,14 @@ class UserController {
   async update(ctx) {
     ctx.verifyParams({
       name: { type: "string", required: false },
-      password: { type: "string", required: false }
+      password: { type: "string", required: false },
+      avatar_url: { type: "string", required: false },
+      gender: { type: "string", required: false },
+      headline: { type: "string", required: false },
+      locations: { type: "array", itemType: "string", required: false },
+      business: { type: "string", required: false },
+      employments: { type: "array", itemType: "object", required: false },
+      educations: { type: "array", itemType: "object", required: false }
     });
     const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body);
     if (!user) {
