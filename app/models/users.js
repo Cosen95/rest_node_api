0889a2e2a37d15fad501a84aedb8067305876995
fengshuan <1263215592@qq.com>
2019-11-02 21:45:55 +0800

    :sparkles: 个人资料schema设计

--- app/models/users.js
+++ app/models/users.js
@@ -5,7 +5,36 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   __v: { type: Number, select: false },
   name: { type: String, required: true },
-  password: { type: String, required: true, select: false }
+  password: { type: String, required: true, select: false },
+  avatar_url: { type: String },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    default: "male",
+    required: true
+  },
+  headline: { type: String },
+  locations: { type: [{ type: String }] },
+  business: { type: String },
+  employments: {
+    type: [
+      {
+        company: { type: String },
+        job: { type: String }
+      }
+    ]
+  },
+  educations: {
+    type: [
+      {
+        school: { type: String },
+        major: { type: String },
+        diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+        entrance_year: { type: Number },
+        graduation_year: { type: Number }
+      }
+    ]
+  }
 });
 
 module.exports = model("User", userSchema);
