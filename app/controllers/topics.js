f6aaf08f6bb0c2c7f04b12579243518777f2d7d2
fengshuan <1263215592@qq.com>
2019-11-04 21:32:43 +0800

    :sparkles: 话题模块增改查接口实现

---
+++ app/controllers/topics.js
@@ -0,0 +1,40 @@
+const Topic = require("../models/topics");
+
+class TopicController {
+  async find(ctx) {
+    ctx.body = await Topic.find();
+  }
+  async findById(ctx) {
+    const { fields } = ctx.query;
+    const selectFields =
+      fields &&
+      fields
+        .split(";")
+        .filter(f => f)
+        .map(f => " +" + f)
+        .join("");
+    const topic = await Topic.findById(ctx.params.id).select(selectFields);
+    ctx.body = topic;
+  }
+  async create(ctx) {
+    ctx.verifyParams({
+      name: { type: "string", required: true },
+      avatar_url: { type: "string", required: false },
+      introduction: { type: "string", required: false }
+    });
+    const topic = await new Topic(ctx.request.body).save();
+    ctx.body = topic;
+  }
+  async update(ctx) {
+    ctx.verifyParams({
+      name: { type: "string", required: false },
+      avatar_url: { type: "string", required: false },
+      introduction: { type: "string", required: false }
+    });
+    const topic = await Topic.findByIdAndUpdate(
+      ctx.params.id,
+      ctx.request.body
+    );
+    ctx.body = topic;
+  }
+}
---
+++ app/models/topics.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const topicSchema = new Schema({
+  __v: { type: Number, select: false },
+  name: { type: String, required: true },
+  avatar_url: { type: String },
+  introduction: { type: String, select: false }
+});
+
+module.exports = model("Topic", topicSchema);
