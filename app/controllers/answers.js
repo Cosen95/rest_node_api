d46d77e9c0974d9791f70a4d8c40dfc8de4ca81f
fengshuan <1263215592@qq.com>
2019-11-11 22:00:56 +0800

    :sparkles: 问题-答案模块models与controlller设计

---
+++ app/controllers/answers.js
@@ -0,0 +1,77 @@
+const Answer = require("../models/answers");
+
+class AnswerController {
+  async find(ctx) {
+    const { per_page = 10 } = ctx.query;
+    const page = Math.max(ctx.query.page * 1, 1) - 1;
+    const perPage = Math.max(per_page * 1, 1);
+    const q = new RegExp(ctx.query.q);
+    ctx.body = await Answer.find({
+      content: q,
+      questionId: ctx.params.questionId
+    })
+      .limit(perPage)
+      .skip(page * perPage);
+  }
+  async findById(ctx) {
+    const { fields } = ctx.query;
+    const selectFields =
+      fields &&
+      fields
+        .split(";")
+        .filter(f => f)
+        .map(f => " +" + f)
+        .join("");
+    const answer = await Answer.findById(ctx.params.id)
+      .select(selectFields)
+      .populate("answerer");
+    ctx.body = answer;
+  }
+  async create(ctx) {
+    ctx.verifyParams({
+      content: { type: "string", required: true }
+    });
+    const answerer = ctx.state.user._id;
+    const { questionId } = ctx.params;
+    const answer = await new Answer({
+      ...ctx.request.body,
+      answerer,
+      questionId
+    }).save();
+    ctx.body = answer;
+  }
+  async checkAnswerer(ctx, next) {
+    const { answer } = ctx.state;
+    if (answer.answerer.toString() !== ctx.state.user._id) {
+      ctx.throw(403, "无权限");
+    }
+    await next();
+  }
+  async update(ctx) {
+    ctx.verifyParams({
+      content: { type: "string", required: false }
+    });
+    await ctx.state.answer.update(ctx.request.body);
+    ctx.body = ctx.state.answer;
+  }
+  async delete(ctx) {
+    const answer = await Answer.findByIdAndRemove(ctx.params.id);
+    if (!answer) {
+      ctx.throw(404, "问题不存在");
+    }
+    ctx.status = 204;
+  }
+  async checkAnswerExist(ctx, next) {
+    const answer = await Answer.findById(ctx.params.id).select("+answerer");
+    if (!answer) {
+      ctx.throw(404, "答案不存在");
+    }
+    if (answer.questionId !== ctx.params.questionId) {
+      ctx.throw(404, "该问题下没有此答案");
+    }
+    ctx.state.answer = answer;
+    await next();
+  }
+}
+
+module.exports = new AnswerController();
---
+++ app/models/answers.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const answerSchema = new Schema({
+  __v: { type: Number, select: false },
+  content: { type: String, required: true },
+  answerer: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    select: false
+  },
+  questionId: { type: String, required: true }
+});
+
+module.exports = model("Answer", answerSchema);
